f9801886d9b5f9d6c97952ff96d07081f3bd67cc
AlphaCeph-C3 <101421534+AlphaCeph-C3@users.noreply.github.com>
2023-08-19 00:09:11 +0530

    completed challenge

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const multer = require('multer');
 
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
+const upload = multer({ dest: 'uploads/' });
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/analyzefile', upload.single('upfile'), function (req, res) {
+  const { originalname: name, mimetype: type, size } = req.file;
+  res.json({ name, type, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
